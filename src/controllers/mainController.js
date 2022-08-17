cbec70658c03a9d3a19391e81906511d0d9529de
ccFabrissin <cayefa@gmail.com>
2022-08-17 16:08:13 -0300

    forms funcionando

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,13 +1,47 @@
 const path = require('path');
 const fs = require('fs');
 
-const nftFilePath = path.resolve(__dirname, '../data/nft.json');
-const nft = JSON.parse(fs.readFileSync(nftFilePath, 'utf-8'));
+const ingresosPath = path.resolve(__dirname, '../data/ingresos.json');
+const ingresos = JSON.parse(fs.readFileSync(ingresosPath, 'utf-8'));
+
+const egresosPath = path.resolve(__dirname,'../data/egresos.json');
+const egresos = JSON.parse(fs.readFileSync(egresosPath, "utf-8"));
+
+
 
 const controladorMain ={
     index: (req,res)=>{
-        res.render('index', {nft:nft})
+        res.render('index')
     },
+    form1: (req, res)=>{
+        let ingreso = {
+            monto: req.body.monto,
+            medio: req.body.medio,
+            fecha: req.body.fecha,
+        };
+        if(ingreso.fecha == ""){
+            ingreso.fecha = new Date;
+        };
+        ingresos.push(ingreso);
+        let ingresosJSON = JSON.stringify(ingresos);
+        fs.writeFileSync(ingresosPath, ingresosJSON);
+        res.redirect("/");
+    },
+    form2: (req, res)=>{
+        let egreso = {
+            concepto: req.body.conceptGast,
+            monto: req.body.montoEg,
+            medio: req.body.medioEg,
+            fecha: req.body.fechaEg
+        };
+        if(egreso.fecha == ""){
+            egreso.fecha = new Date;
+        };
+        egresos.push(egreso);
+        let egresosJSON = JSON.stringify(egresos);
+        fs.writeFileSync(egresosPath, egresosJSON);
+        res.redirect("/");
+    }
 }
 
 module.exports = controladorMain;
--- src/routes/main.js
+++ src/routes/main.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const mainController = require ('../controllers/mainController')
 
 router.get('/', mainController.index);
+router.post('/form1', mainController.form1);
+router.post('/form2', mainController.form2);
 
 module.exports = router;
